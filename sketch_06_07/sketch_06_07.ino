f5d800564d1728271d4e77211c6307265e8c8bf8
Ryan Brideau <ryan.brideau@gmail.com>
2015-04-21 22:28:31 -0300

    Updated bounce library

---
+++ sketch_06_07/sketch_06_07.ino
@@ -0,0 +1,24 @@
+#include <Bounce2.h>
+
+int inputPin = 5;
+int ledPin = 13;
+
+int ledValue = LOW;
+Bounce bouncer = Bounce();
+
+void setup() {
+ pinMode(ledPin, OUTPUT);
+
+ pinMode(inputPin, INPUT);
+ digitalWrite(inputPin, HIGH);
+ 
+ bouncer.attach(inputPin);
+ bouncer.interval(5);
+}
+
+void loop() {
+  if (bouncer.update() && bouncer.read() == LOW) {
+   ledValue = ! ledValue;
+   digitalWrite(ledPin, ledValue);
+  }
+}
